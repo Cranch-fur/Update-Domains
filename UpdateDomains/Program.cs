cb5860b43a658a6977c0135d62b119ab26295868
Cranch <50642181+Cranch-fur@users.noreply.github.com>
2024-11-12 19:03:55 +0300

    1.1.0.0
    - Улучшена производительность при работе с большими массивами данных (повсеместное использование HashSet<string>);
    - Добавлена запись .log файла, не более одного файла (старый файл будет перезаписываться при каждом запуске программы);
    - Была убрана лишняя новая строка.

--- UpdateDomains/Program.cs
+++ UpdateDomains/Program.cs
@@ -1,4 +1,7 @@
-﻿using CranchyLib.Networking; // Source: https://github.com/Cranch-fur/CranchyLib.Networking
+﻿// Update Domains
+// https://github.com/Cranch-fur/Update-Domains
+
+using CranchyLib.Networking; // Source: https://github.com/Cranch-fur/CranchyLib.Networking
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,6 +15,7 @@ namespace UpdateDomains
         internal const string gDPILocalDomainsFilePath   = "russia-blacklist.txt";
         internal const string zapretLocalDomainsFilePath = "list-general.txt";
         internal const string defaultDomainsFilePath     = "Default Domains.txt";
+        internal const string logFilePath                = "Update Domains.log";
 
 
 
@@ -24,29 +28,39 @@ namespace UpdateDomains
 
             Environment.Exit(0);
         }
+
+
+        static void FlushLog() => File.WriteAllText(logFilePath, string.Empty);
+        static void WriteLog(string message)
+        {
+            Console.WriteLine(message);
+            File.AppendAllText(logFilePath, $"{message}\n");
+        }
+
+
         static void CheckDefaultDomains()
         {
             if (File.Exists(defaultDomainsFilePath) == false)
             {
-                Console.WriteLine($"[ERROR] {defaultDomainsFilePath} File is missing!");
+                WriteLog($"[ERROR] {defaultDomainsFilePath} File is missing!");
                 Exit();
             }
         }
-        static void PopulateFile(string filePath, List<string> domainsList)
+        static void PopulateFile(string filePath, HashSet<string> domainsList)
         {
-            Console.WriteLine("\n\n");
-            List<string> newDomainsList = new List<string>(domainsList); // Further in the code we will be making changes to List we've received on function execution. Variables from function execution are references to already existing ones, since we don't want to ruin our domainsList, we're making up a new one.
+            WriteLog("\n\n");
+            HashSet<string> newDomainsList = new HashSet<string>(domainsList); // Further in the code we will be making changes to HashSet we've received on function execution. Variables from function execution are references to already existing ones, since we don't want to ruin our domainsList, we're making up a new one.
 
 
             if (File.Exists(filePath) == true)
             {
-                Console.WriteLine($"[~] \"{filePath}\" File was found!");
-                Console.WriteLine("[~] Attempting to update the file...");
+                WriteLog($"[~] \"{filePath}\" File was found!");
+                WriteLog("[~] Attempting to update the file...");
                 Thread.Sleep(3000); // Intentional slow-down to let user read LOG we've just printed.
 
 
-                HashSet<string> localDomainsHashSet = new HashSet<string>(File.ReadAllLines(filePath)); // As soon as we read file lines, convert them to HashSet data type. HashSet allows us to effectively iterate through thousands of elements.
-                List<string> uniqueDomains = new List<string>(); // List of domains we consider "unique" - those domains aren't present in file yet.
+                HashSet<string> localDomainsHashSet = new HashSet<string>(File.ReadAllLines(filePath));
+                HashSet<string> uniqueDomains = new HashSet<string>(); // HashSet of domains we consider "unique" - those domains aren't present in file yet.
 
                 foreach (string domain in newDomainsList)
                 {
@@ -60,96 +74,100 @@ namespace UpdateDomains
                 {
                     foreach (string domain in uniqueDomains)
                     {
-                        Console.WriteLine($"[+] {domain}");
+                        WriteLog($"[+] {domain}");
                         localDomainsHashSet.Add(domain);
                     }
 
-                    List<string> outDomainsList = localDomainsHashSet.OrderBy(item => item).ToList(); 
+                    HashSet<string> outDomainsList = new HashSet<string>(localDomainsHashSet.OrderBy(item => item)); 
                     File.WriteAllLines(filePath, outDomainsList);
 
 
-                    Console.WriteLine($"[~] \"{filePath}\" File successfully updated!");
+                    WriteLog($"[~] \"{filePath}\" File successfully updated!");
                 }
                 else
                 {
-                    Console.WriteLine("[~] There's no new domains to add, your file is already up to date!");
+                    WriteLog("[~] There's no new domains to add, your file is already up to date!");
                 }
             }
             else
             {
-                Console.WriteLine($"[~] \"{filePath}\" File wasn't found!");
-                Console.WriteLine("[~] Attempting to create the file...");
+                WriteLog($"[~] \"{filePath}\" File wasn't found!");
+                WriteLog("[~] Attempting to create the file...");
                 Thread.Sleep(3000); // Intentional slow-down to let user read LOG we've just printed.
 
 
                 CheckDefaultDomains();
 
 
-                List<string> defaultDomains = File.ReadAllLines(defaultDomainsFilePath).ToList();
-                foreach (string defaultDomain in defaultDomains)
+                List<string> defaultDomains = new List<string>(File.ReadAllLines(defaultDomainsFilePath));
+                foreach (string domain in defaultDomains)
                 {
-                    newDomainsList.Add(defaultDomain);
+                    newDomainsList.Add(domain);
                 }
 
-                List<string> outDomainsList = newDomainsList.OrderBy(item => item).ToList(); // Sort List entries.
+                HashSet<string> outDomainsList = new HashSet<string>(newDomainsList.OrderBy(item => item)); // Sort List entries.
                 File.WriteAllLines(filePath, outDomainsList);
 
 
-                Console.WriteLine($"[~] \"{filePath}\" File successfully created!");
+                WriteLog($"[~] \"{filePath}\" File successfully created!");
             }
         }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine(" ██████╗ ██████╗ ██████╗ ██╗        ██╗    ███████╗ █████╗ ██████╗ ██████╗ ███████╗████████╗");
-            Console.WriteLine("██╔════╝ ██╔══██╗██╔══██╗██║       ██╔╝    ╚══███╔╝██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝");
-            Console.WriteLine("██║  ███╗██║  ██║██████╔╝██║      ██╔╝       ███╔╝ ███████║██████╔╝██████╔╝█████╗     ██║   ");
-            Console.WriteLine("██║   ██║██║  ██║██╔═══╝ ██║     ██╔╝       ███╔╝  ██╔══██║██╔═══╝ ██╔══██╗██╔══╝     ██║   ");
-            Console.WriteLine("╚██████╔╝██████╔╝██║     ██║    ██╔╝       ███████╗██║  ██║██║     ██║  ██║███████╗   ██║   ");
-            Console.WriteLine(" ╚═════╝ ╚═════╝ ╚═╝     ╚═╝    ╚═╝        ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝   ╚═╝   ");
-            Console.WriteLine("Domains list updating tool");
-            Console.WriteLine("Originator: bropines");
-            Console.WriteLine("Tool by: Cranch (Кранч) The Wolf");
-            Console.WriteLine("\n\n");
+            Console.BufferHeight = short.MaxValue - 1; // Set console height buffer size to maximum available.
+            FlushLog();
+
+            WriteLog(" ██████╗ ██████╗ ██████╗ ██╗        ██╗    ███████╗ █████╗ ██████╗ ██████╗ ███████╗████████╗");
+            WriteLog("██╔════╝ ██╔══██╗██╔══██╗██║       ██╔╝    ╚══███╔╝██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝");
+            WriteLog("██║  ███╗██║  ██║██████╔╝██║      ██╔╝       ███╔╝ ███████║██████╔╝██████╔╝█████╗     ██║   ");
+            WriteLog("██║   ██║██║  ██║██╔═══╝ ██║     ██╔╝       ███╔╝  ██╔══██║██╔═══╝ ██╔══██╗██╔══╝     ██║   ");
+            WriteLog("╚██████╔╝██████╔╝██║     ██║    ██╔╝       ███████╗██║  ██║██║     ██║  ██║███████╗   ██║   ");
+            WriteLog(" ╚═════╝ ╚═════╝ ╚═╝     ╚═╝    ╚═╝        ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝   ╚═╝   ");
+            WriteLog("Domains list updating tool");
+            WriteLog("Originator: bropines");
+            WriteLog("Tool by: Cranch (Кранч) The Wolf");
+            WriteLog("\n\n");
 
 
-            Console.WriteLine("[~] Attempting to fetch domains...");
+            WriteLog("[~] Attempting to fetch domains...");
 
 
             var getDomainsResponse = Networking.Get("https://zapret.cranchpalace.info/getDomains", 300);
             if (getDomainsResponse.statusCode == Networking.E_StatusCode.OK)
             {
                 string getDomainsContent = getDomainsResponse.content;
-                List<string> getDomainsList = new List<string>(getDomainsContent.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries));
-                Console.WriteLine($"[~] Domains fetched: {getDomainsList.Count}");
+                HashSet<string> getDomainsList = new HashSet<string>(getDomainsContent.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries));
+                WriteLog($"[~] Domains fetched: {getDomainsList.Count}");
 
 
-                Console.WriteLine("[~] Scanning for Goodbye DPI...");
+                WriteLog("[~] Scanning for Goodbye DPI...");
                 if (Directory.Exists("x86")) // Directory included with: https://github.com/ValdikSS/GoodbyeDPI
                 {
-                    Console.WriteLine("[~] Goodbye DPI detected.");
+                    WriteLog("[~] Goodbye DPI detected.");
                     PopulateFile(gDPILocalDomainsFilePath, getDomainsList);
                 }
                 else
                 {
-                    Console.WriteLine("[~] Goodbye DPI wasn't found.");
+                    WriteLog("[~] Goodbye DPI wasn't found.");
                 }
 
 
-                Console.WriteLine("[~] Scanning for Zapret...");
+                WriteLog("[~] Scanning for Zapret...");
                 if (Directory.Exists("bin")) // Directory included with: https://github.com/Flowseal/zapret-discord-youtube
                 {
-                    Console.WriteLine("[~] Zapret detected.");
+                    WriteLog("[~] Zapret detected.");
                     PopulateFile(zapretLocalDomainsFilePath, getDomainsList);
                 }
                 else
                 {
-                    Console.WriteLine("[~] Zapret wasn't found.");
-                    Console.WriteLine("");
+                    WriteLog("[~] Zapret wasn't found.");
                 }
             }
             else
             {
-                Console.WriteLine("[ERROR] Failed to fetch domains.");
+                WriteLog("[ERROR] Failed to fetch domains.");
             }
 
 
